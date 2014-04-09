9488c6604e936cd43c80580992f2be3429949584
Emily Rounds <roundse@uci.edu>
2014-04-09 16:53:34 -0700

    Added some new cycle files. Probably going to have to change the queues next.

---
+++ cycle_food_pfc.m
@@ -0,0 +1,55 @@
+function returnable = cycle_food_pfc(food_in, input_weights, input, value)
+    global w_food_to_food;
+    global VAL;
+
+    global food_in_queue;
+    global food_weight_queue;
+    
+    global FOOD_CELLS;
+    
+    % recurrency stuff
+    global w_food_to_pfc;
+    global w_pfc_to_food;
+       
+    food_eye = eye(FOOD_CELLS);
+      
+    queue_pos = length(food_in_queue)+1;
+    
+    if nargin < 3
+    	total_inputs = 0;
+        pfc_in = food_in{2};
+        food_in = food_in{1};
+        
+    	for i = 1:(queue_pos-1)
+    		total_inputs = total_inputs + food_in_queue{i} * food_weight_queue{i};
+        end
+
+        food_out = activity(food_in, food_eye, total_inputs, ...
+            w_food_to_food);
+        
+        
+        returnable = food_out;
+
+        if input_weights
+                [w_pfc_to_food w_food_to_pfc] = recurrent_oja(food_out, food_in, pfc_in, ...
+                    w_pfc_to_food, w_food_to_pfc, VAL, 1);
+                
+        end
+
+        food_in_queue = {};
+    else
+        % return the weights given if no weight in queue
+        if ( queue_pos > length(food_weight_queue) )
+            returnable = input_weights;
+        else
+            returnable = food_weight_queue{queue_pos};
+        end
+        
+        if nargin > 3
+            VAL = value;
+        end
+        
+        food_in_queue{queue_pos} = input;
+        food_weight_queue{queue_pos} = input_weights;
+    end 
+end
---
+++ cycle_food_place.m
@@ -0,0 +1,53 @@
+function returnable = cycle_food_place(place_in, input_weights, input, value)
+global w_place_to_place;
+global PVAL;
+
+global place_in_queue;
+global place_weight_queue;
+
+global PLACE_CELLS;
+PLACE_CELLS = 14;
+
+% recurrency stuff
+global w_food_to_place;
+global w_place_to_food;
+
+place_eye = eye(PLACE_CELLS);
+w_place_to_place = zeros(PLACE_CELLS);
+
+queue_pos = length(place_in_queue)+1;
+
+if nargin < 3
+    total_inputs = 0;
+    hpc_in = place_in{2};
+    place_in = place_in{1};
+    
+    for i = 1:(queue_pos-1)
+        total_inputs = total_inputs + place_in_queue{i} * place_weight_queue{i};
+    end
+    
+    place_out = activity(place_in, place_eye, total_inputs, ...
+        w_place_to_place);
+    
+    returnable = place_out;
+    
+    if input_weights
+    [w_food_to_place w_place_to_food] = recurrent_oja(place_out, place_in, ...
+        hpc_in, w_food_to_place, w_place_to_food, PVAL, 1);
+    end
+    
+    place_in_queue = {};
+else
+    % return the weights given if no weight in queue
+    if ( queue_pos > length(place_weight_queue) )
+        returnable = input_weights;
+    else
+        returnable = place_weight_queue{queue_pos};
+    end
+    
+    PVAL = value;
+    
+    place_in_queue{queue_pos} = input;
+    place_weight_queue{queue_pos} = input_weights;
+end
+end
